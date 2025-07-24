e362f9784f04af5f8493604e3da1820d4f206f63
Mruday0409 <mruday2761@gmail.com>
2025-07-24 17:36:22 +0530

    commit nav barstyle

--- src/components/OrganNavbar.jsx
+++ src/components/OrganNavbar.jsx
@@ -1,5 +1,5 @@
 // src/components/OrganNavbar.jsx
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link, useLocation } from "react-router-dom";
 import "../styles/OrganNavbar.css";
 import logo from "../assets/images/Anatoscope-logo.png";
@@ -19,16 +19,23 @@ const organs = [
   { name: "Heart", icon: heartIcon, path: "/heart" },
   { name: "Kidney", icon: kidneyIcon, path: "/kidney" },
   { name: "Liver", icon: liverIcon, path: "/liver" },
-  { name: "UrinaryBladder", icon: bladderIcon, path: "/UrinaryBladder" },
+  { name: "Urinary Bladder", icon: bladderIcon, path: "/urinary-bladder" },
 ];
 
 const OrganNavbar = () => {
   const location = useLocation();
-  const [activePath, setActivePath] = useState(location.pathname);
+  const centerRef = useRef(null);
+  const [beamLeft, setBeamLeft] = useState(0);
 
   useEffect(() => {
-    setActivePath(location.pathname);
-  }, [location]);
+    const activeIcon = centerRef.current?.querySelector(".organ-icon-wrapper.active");
+    if (activeIcon) {
+      const parentRect = centerRef.current.getBoundingClientRect();
+      const iconRect = activeIcon.getBoundingClientRect();
+      const left = iconRect.left - parentRect.left + iconRect.width / 2;
+      setBeamLeft(left);
+    }
+  }, [location.pathname]);
 
   return (
     <nav className="organ-navbar">
@@ -39,21 +46,19 @@ const OrganNavbar = () => {
         </Link>
       </div>
 
-      <div className="organ-navbar-center">
+      <div className="organ-navbar-center" ref={centerRef}>
+        <div
+          className="beam-slider"
+          style={{ left: `${beamLeft}px` }}
+        ></div>
         {organs.map((organ) => (
           <div
-            className={`organ-icon-wrapper ${
-              activePath === organ.path ? "active" : ""
-            }`}
+            className={`organ-icon-wrapper ${location.pathname === organ.path ? "active" : ""}`}
             key={organ.name}
           >
             <div className="spotlight"></div>
             <Link to={organ.path}>
-              <img
-                src={organ.icon}
-                alt={organ.name}
-                className="organ-icon"
-              />
+              <img src={organ.icon} alt={organ.name} className="organ-icon" />
             </Link>
           </div>
         ))}

847bb7a0c0011458e2bec870df0609b7d5283a5b
Mruday0409 <mruday2761@gmail.com>
2025-07-24 11:41:55 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { motion, AnimatePresence } from "framer-motion";
-// import HomeNavbar from "../components/HomeNavbar";
-import HomeNavbar from "../components/OrganNavbar";
+import HomeNavbar from "../components/HomeNavbar";
+
 import "../styles/Home.css";
 
 // ✅ All videos centralized here
